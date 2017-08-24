1f6b84f53e0004698957fb9da4f2ec130ddd1694
yk1028 <wongeunsong94@gmail.com>
2017-08-24 16:12:00 +0900

    add user's img,name in chat

--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,8 @@ ActiveRecord::Schema.define(version: 20170821125524) do
   create_table "pushes", force: :cascade do |t|
     t.integer "user_id"
     t.string "message"
-    t.integer "group_id"
+    t.integer "pusher_id"
+    t.boolean "isGroup"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_pushes_on_user_id"
